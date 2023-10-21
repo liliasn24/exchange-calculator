460cc2b59293f768a63f1d00cb0e350ceafbd9f6
= <salaslilia00@gmail.com>
2023-10-20 20:53:59 -0400

    created events-dom elements

---
+++ script.js
@@ -0,0 +1,11 @@
+const currencyEl_one = document.getElementById("currency-one");
+const amountEl_one = document.getElementById("amount-one");
+const currencyEl_two = document.getElementById("currency-two");
+const amountEl_two = document.getElementById("amount-one");
+
+const rateEl = document.getElementById("rate");
+const swap = document.getElementById("swap");
+
+function calculate() {}
+
+calculate();
