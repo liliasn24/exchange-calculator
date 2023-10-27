f78f6065011de4f34b0822d4786d6072c6356c15
= <salaslilia00@gmail.com>
2023-10-26 22:28:42 -0400

    api

--- script.js
+++ script.js
@@ -7,7 +7,16 @@ const rateEl = document.getElementById("rate");
 const swap = document.getElementById("swap");
 
 //fetch exchange rate and update the DOM
-function calculate() {}
+function calculate() {
+  const curreny_one = currencyEl_one.value;
+  const currency_two = currencyEl_two.value;
+
+  fetch()
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    });
+}
 
 //event listeners
 currencyEl_one.addEventListener("change", calculate);
